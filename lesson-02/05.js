ebb45e974a75a574d9e6cf5e18209f6ebd43780f
4DDesigner <95ejey@mail.ru>
2024-01-10 20:06:05 +0300

    2

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,7 +2,7 @@ let passport = {
     name: "Petr",
     surname: "Petrov"
 };
-console.log(passport);
+console.log(passport.name + passport.surname);
 let copyPassport = JSON.parse(JSON.stringify(passport));
 copyPassport.name = 'Ivan';
-console.log(copyPassport);
+console.log(copyPassport.name + copyPassport.surname);
