85514131d2f79d16c229eedcb04fdc03c93e9697
4DDesigner <95ejey@mail.ru>
2024-01-15 23:04:12 +0300

    lesson-3_01_3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+function createColorString(item) {
+    return item  === "черный" || item  === "красный" || item  === "желтый"
+}
+console.log(colors.filter(createColorString).join('-'));
+
+
+
+
+
+
