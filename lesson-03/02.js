43bd54b212ef9735ddd87107bebdd34762acad7f
4DDesigner <95ejey@mail.ru>
2024-01-16 19:05:31 +0300

    lesson-3_02

---
+++ lesson-03/02.js
@@ -0,0 +1,7 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+let found =  words.indexOf("апельсин");
+if( found >= 0) 
+console.log("Ура! нашел");
+else {
+    console.log("Придется поискать в другом магазине…");
+}
